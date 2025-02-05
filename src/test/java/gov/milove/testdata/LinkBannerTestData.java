d67e8b535e3f2e0e515d8802f57c374298dc978f
LiashenkoAndrey <a.liashenko@e-u.edu.ua>
2025-02-05 14:48:08 +0200

    add unit tests

--- src/test/java/gov/milove/testdata/LinkBannerTestData.java
+++ src/test/java/gov/milove/testdata/LinkBannerTestData.java
@@ -7,11 +7,7 @@ public class LinkBannerTestData {
   }
 
   public static final Long LINK_BANNER_ID = 1L;
-  public static final String LINK_BANNER_TEXT = "Test Banner";
-  public static final String LINK_BANNER_URL = "http://example.com";
 
   public static final Long LINK_BANNER_FOR_DELETION_ID = 2L;
-  public static final String LINK_BANNER_FOR_DELETION_TEXT = "Delete Banner";
-  public static final String LINK_BANNER_FOR_DELETION_URL = "http://delete.com";
 
 }
